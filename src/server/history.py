43cde3952e5065a09701cad222825c997f2875f1
z5371689 <michael.he@student.unsw.edu.au>
2022-07-16 23:30:53 +1000

    Implement adding an entry to History

--- src/server/history.py
+++ src/server/history.py
@@ -11,7 +11,23 @@ class History:
         self.__database = database
         self.__users = database["users"]
     
-
     def __save_to_database(self):
         with open(self.filename, "w+") as f:
             json.dump(self.__database, f)
+    
+    def add(self, uuid, title, source, score, author, date_added):
+        """Add a new article as read by user `uuid`."""
+        if uuid not in self.__users:
+            self.__users[uuid] = []
+        
+        self.__users[uuid].append({
+            "title": title,
+            "source": source,
+            "score": score,
+            "author": author,
+            "date_added": date_added
+        })
+
+        self.__save_to_database()
+        
+        return "success"
