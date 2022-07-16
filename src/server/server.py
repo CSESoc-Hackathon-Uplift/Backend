cb5e082fed2628abf066632252f9099d39ff3595
z5371689 <michael.he@student.unsw.edu.au>
2022-07-16 23:32:41 +1000

    Refactor /news and /news/headlines routes

--- src/server/server.py
+++ src/server/server.py
@@ -11,6 +11,27 @@ with open("news.json") as f:
 def root():
     return "<h1 style='font-family: Arial'; font-size: 60px;'>Welcome to Uplift's backend API!</h1>"
 
+@APP.route('/news', methods=["GET"])
+def get_news():
+    search = request.args.get("search")
+    return requests.get(f"{news['base_url']}/v2/everything?apiKey={news['api_key']}&q={search}").json()
+
+@APP.route('/news/headlines', methods=["GET"])
+def get_news_headlines():
+    search = request.args.get("search")
+    category = request.args.get("category")
+    country = request.args.get("country")
+    query_string = ""
+    
+    if search:
+        query_string += f"&q={search}"
+    if category:
+        query_string += f"&category={category}"
+    if country:
+        query_string += f"&country={country}"
+    # print(f"{query_string=}")
+    return requests.get(f"{news['base_url']}/v2/top-headlines?apiKey={news['api_key']}{query_string}").json()
+
 @APP.route('/history/add', methods=["POST"])
 def history_add():
     history = History("database.json")
@@ -24,3 +45,5 @@ def history():
     return {
         "history": history.visited(uuid)
     }
+
+APP.run(host="0.0.0.0", debug=True, port=8080)
