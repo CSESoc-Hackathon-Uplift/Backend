4385a1da00313c62404213a3234ae4f02be7fa63
z5371689 <michael.he@student.unsw.edu.au>
2022-07-16 23:32:22 +1000

    Implement /history/add and /history routes

--- src/server/server.py
+++ src/server/server.py
@@ -11,3 +11,16 @@ with open("news.json") as f:
 def root():
     return "<h1 style='font-family: Arial'; font-size: 60px;'>Welcome to Uplift's backend API!</h1>"
 
+@APP.route('/history/add', methods=["POST"])
+def history_add():
+    history = History("database.json")
+    body = request.json()
+    return history.add(body["uuid"], body["title"], body["source"], body["score"], body["author"], body["date_added"])
+
+@APP.route('/history', methods=["GET"])
+def history():
+    uuid = request.args.get("uuid")
+    history = History("database.json")
+    return {
+        "history": history.visited(uuid)
+    }
