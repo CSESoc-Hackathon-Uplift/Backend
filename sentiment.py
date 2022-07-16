623f3704e7f05e279b236c2ac4fb7765861c9c3c
z5371689 <michael.he@student.unsw.edu.au>
2022-07-16 17:51:29 +1000

    Initialise client for Google Language Service

--- sentiment.py
+++ sentiment.py
@@ -4,3 +4,16 @@ import json
 
 def analyse_sentiment_textblob(text_content):
     return TextBlob(text_content).sentiment.polarity
+
+def analyse_sentiment_google(text_content):
+    """
+    Perform sentiment analysis on text.
+
+    Args:
+        text_content (str): The text content to analyse.
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
